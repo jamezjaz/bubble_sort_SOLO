7f4af065e60885f484dd6830eaf87ced028f25c4
Jamezjaz <jamezjaz@gmail.com>
2020-06-18 11:01:42 -0500

    Implements lint errors

--- index.rb
+++ index.rb
@@ -1,16 +1,16 @@
 def bubble_sort(array)
-    i = 0
-    j = 0
-    len = array.length
-    until i == len
-        j = i + 1
-        while j < len
-            array[i], array[j] = array[j], array[i] if array[i] > array[j]
-            j += 1
-        end
-        i += 1
+  i = 0
+  j = 0
+  len = array.length
+  until i == len
+    j = i + 1
+    while j < len
+      array[i], array[j] = array[j], array[i] if array[i] > array[j]
+      j += 1
     end
-    array
+    i += 1
+  end
+  array
 end
 
 array = [2, 5, 7, 2, 8, 1, 100, 0, 6, 9, 35, 3]
